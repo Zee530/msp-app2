eeb65e1d1dc95844953d67f68b5d630ec4ecd62f
Zee530 <Zeee530@gmail.com>
2023-06-23 22:16:39 +0100

    select

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import {
   Link,
   List,
   ListItem,
+  Select,
   Text,
 } from "@chakra-ui/react";
 
@@ -13,24 +14,28 @@ function App() {
   const buttonStyle:React.CSSProperties = {
     color: "white",
     background: "inherit",
+    //To be removed when i figure out custom button theming
   };
+
+  const computeButton:React.CSSProperties = {
+    top: '30px',
+    backgroundColor: '#2e51a2',
+    color: 'white',
+    width: '250px',
+    height: '50px'
+  }
 
   const seasons:string[] = ['WINTER', 'SPRING', 'SUMMER', 'FALL']
 
   return (
     <Container maxW="container.xl">
-      {/* <Center> */}
       <Text
-        // bgGradient="linear(to-r, green, red)"
-        // bgClip="text"
         fontSize="4xl"
         fontWeight="normal"
-        // color='white'
         fontFamily="ultra, sans-serif"
       >
         MAL Score Progressions
       </Text>
-      {/* </Center> */}
       <Box bgColor="#2e51a2">
         <Center>
           <List display="flex" justifyContent="space-around" w="50%">
@@ -42,6 +47,18 @@ function App() {
             </Link>  
             )}
           </List>
+        </Center>
+      </Box>
+      <Box mt={100}>
+        <Center>
+        <Select size='lg' w='70%' placeholder="Select Anime">
+          <option>Anime 1</option>
+          <option>Anime 2</option>
+          <option>Anime 3</option>
+        </Select>
+        </Center>
+        <Center>
+            <Button style={computeButton}>COMPUTE</Button>
         </Center>
       </Box>
     </Container>
---
+++ src/shows.ts
@@ -0,0 +1,1 @@
+const shows:string[] = ['tbd']
