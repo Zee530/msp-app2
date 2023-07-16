2e811aaf5950a780692086a7067224966072de7e
Zee530 <Zeee530@gmail.com>
2023-07-16 18:12:55 +0100

    Textarea, Stack spacing and type declarations

--- src/App.tsx
+++ src/App.tsx
@@ -7,76 +7,100 @@ import {
   List,
   ListItem,
   Select,
+  Stack,
   Text,
+  Textarea,
 } from "@chakra-ui/react";
 import React from "react";
 
 function App() {
-  const [anime, setAnime] = React.useState('')
+  const [anime, setAnime] = React.useState("");
+  const [threadData, setThreadData] = React.useState("");
 
-  const handleChange = (e:React.ChangeEvent<HTMLSelectElement>) => {
-    setAnime(e.target.value)
-  }
+  const selecthandleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setAnime(e.target.value);
+  };
 
-  const buttonStyle:React.CSSProperties = {
+  const textareahandleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setThreadData(e.target.value);
+  };
+  const buttonStyle: React.CSSProperties = {
     color: "white",
     background: "inherit",
     //To be removed when i figure out custom button theming
   };
 
-  const computeButton:React.CSSProperties = {
-    top: '30px',
-    backgroundColor: '#2e51a2',
-    color: 'white',
-    width: '250px',
-    height: '50px'
-  }
+  const computeButton: React.CSSProperties = {
+    top: "30px",
+    backgroundColor: "#2e51a2",
+    color: "white",
+    width: "250px",
+    height: "50px",
+  };
 
-  const seasons:string[] = ['WINTER', 'SPRING', 'SUMMER', 'FALL']
-
+  const seasons: string[] = ["WINTER", "SPRING", "SUMMER", "FALL"];
 
   function compute() {
-    if (anime === '') {
-      alert('Select an anime below')
+    if (anime === "") {
+      alert("Select an anime below");
     } else {
-      alert(`You have clicked on ${anime}`)
+      alert(`You have clicked on ${anime}`);
     }
   }
 
   return (
     <Container maxW="container.xl">
-      <Text
-        fontSize="4xl"
-        fontWeight="normal"
-        fontFamily="ultra, sans-serif"
-      >
-        MAL Score Progressions
-      </Text>
-      <Box bgColor="#2e51a2">
-        <Center>
-          <List display="flex" justifyContent="space-around" w="50%">
-            {seasons.map((season) => 
-              <Link>
-              <ListItem>
-                <Button style={buttonStyle}>{season}</Button>
-              </ListItem>
-            </Link>  
-            )}
-          </List>
-        </Center>
-      </Box>
-      <Box mt={100}>
-        <Center>
-        <Select size='lg' w='70%' placeholder="Select Anime" color='grey' value={anime} onChange={handleChange}>
-          <option value='anime1'>Anime 1</option>
-          <option value='anime2'>Anime 2</option>
-          <option value='anime3'>Anime 3</option>
-        </Select>
-        </Center>
-        <Center>
-            <Button style={computeButton} onClick={compute}>COMPUTE</Button>
-        </Center>
-      </Box>
+      <Stack direction='column' spacing={20}>
+        <Stack spacing={0}>
+        <Text fontSize="4xl" fontWeight="normal" fontFamily="ultra, sans-serif">
+          MAL Score Progressions
+        </Text>
+        <Box bgColor="#2e51a2">
+          <Center>
+            <List display="flex" justifyContent="space-around" w="50%">
+              {seasons.map((season) => (
+                <Link>
+                  <ListItem>
+                    <Button style={buttonStyle}>{season}</Button>
+                  </ListItem>
+                </Link>
+              ))}
+            </List>
+          </Center>
+        </Box>
+        </Stack>
+        <Box>
+          <Center>
+            <Select
+              size="lg"
+              w="70%"
+              placeholder="Select Title"
+              color="grey"
+              value={anime}
+              onChange={selecthandleChange}
+            >
+              <option value="anime1">Title 1</option>
+              <option value="anime2">Title 2</option>
+              <option value="anime3">Title 3</option>
+            </Select>
+          </Center>
+          <Center>
+            <Button style={computeButton} onClick={compute}>
+              COMPUTE
+            </Button>
+          </Center>
+        </Box>
+        <Box>
+          <Center>
+          <Textarea
+            value={threadData}
+            onChange={textareahandleChange}
+            placeholder="Data will be displayed here"
+            w='50%'
+          />
+          </Center>
+        </Box>
+      </Stack>
     </Container>
   );
 }
--- src/shows.ts
+++ src/shows.ts
@@ -1,1 +1,21 @@
-const shows:string[] = ['tbd']
+type Show = {
+    title: string,
+    score: number,
+    members: number
+}
+
+const animes: Show[] = [{
+    title: 'Naruto',
+    score: 7.98,
+    members: 1235795
+},
+{
+    title: 'Bleach',
+    score: 8.22,
+    members: 985213
+},
+{
+    title: 'One Piece',
+    score: 8.44,
+    members: 1085234
+},] 
