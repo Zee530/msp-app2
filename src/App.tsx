502a674a83d02ed3e9c37595bb54b8bf30b8557e
Zee530 <Zeee530@gmail.com>
2023-11-19 21:41:47 +0100

    array computation

--- src/App.tsx
+++ src/App.tsx
@@ -15,12 +15,14 @@ import React from "react";
 import titles, { Show } from "./shows";
 
 function App() {
-  const [anime, setAnime] = React.useState("");
-  const [threadData, setThreadData] = React.useState("");
-  const [selectedTitle, setSelectedTitle] = React.useState<Show | null>(null)
+  // const [anime, setAnime] = React.useState("");
+  // const [threadData, setThreadData] = React.useState("");
+  const [selectedTitle, setSelectedTitle] = React.useState<Show | null>(null);
+  const [textareaContent, setTextareaContent] = React.useState('')
 
   const selecthandleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setAnime(e.target.value);
+    const title = titles.find(t => t.title === e.target.value);
+    setSelectedTitle(title ? title : null);
   };
 
   // const textareahandleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -43,34 +45,38 @@ function App() {
   const seasons: string[] = ["WINTER", "SPRING", "SUMMER", "FALL"];
 
   function compute() {
-    if (anime === "") {
+    if (selectedTitle === null) {
       alert("Select an anime below");
     } else {
       // setArrayValue()
-      setThreadData(`${titles[0].title} - ${titles[0].score}, ${titles[0].members}`)
+      setTextareaContent(`Week 01 - ${selectedTitle?.score}, ${selectedTitle?.members} Members`)
     }
   }
 
   return (
     <Container maxW="container.xl">
-      <Stack direction='column' spacing={20}>
+      <Stack direction="column" spacing={20}>
         <Stack spacing={0}>
-        <Text fontSize="4xl" fontWeight="normal" fontFamily="ultra, sans-serif">
-          MAL Score Progressions
-        </Text>
-        <Box bgColor="#2e51a2">
-          <Center>
-            <List display="flex" justifyContent="space-around" w="50%">
-              {seasons.map((season) => (
-                <Link>
-                  <ListItem>
-                    <Button style={buttonStyle}>{season}</Button>
-                  </ListItem>
-                </Link>
-              ))}
-            </List>
-          </Center>
-        </Box>
+          <Text
+            fontSize="4xl"
+            fontWeight="normal"
+            fontFamily="ultra, sans-serif"
+          >
+            MAL Score Progressions
+          </Text>
+          <Box bgColor="#2e51a2">
+            <Center>
+              <List display="flex" justifyContent="space-around" w="50%">
+                {seasons.map((season) => (
+                  <Link>
+                    <ListItem>
+                      <Button style={buttonStyle}>{season}</Button>
+                    </ListItem>
+                  </Link>
+                ))}
+              </List>
+            </Center>
+          </Box>
         </Stack>
         <Box>
           <Center>
@@ -79,12 +85,13 @@ function App() {
               w="70%"
               placeholder="Select Title"
               color="grey"
-              value={anime}
               onChange={selecthandleChange}
             >
-              {titles.map(( title, index) => {
-                return <option key={index} value={title.score}>{title.title}</option>
-              })}
+              {titles.map((title, index) => (
+                <option key={index} value={title.title}>
+                  {title.title}
+                </option>
+              ))}
               {/* <option value="anime1">Title 1</option>
               <option value="anime2">Title 2</option>
               <option value="anime3">Title 3</option> */}
@@ -98,13 +105,21 @@ function App() {
         </Box>
         <Box>
           <Center>
-          <Textarea
-            value={threadData}
-            // value = {titles[0].members}
-            // onChange={textareahandleChange}
-            placeholder="Data will be displayed here"
-            w='50%'
-          />
+            {/* {selectedTitle && (
+              <Textarea
+                value={`Week 01 - ${selectedTitle.score}, ${selectedTitle.members}`}
+                // value = {titles[0].members}
+                // onChange={textareahandleChange}
+                placeholder="Data will be displayed here"
+                w="50%"
+                readOnly
+              />
+            )} */}
+            <Textarea 
+              value={textareaContent} 
+              placeholder="Data will be displayed here"
+              w="50%"
+              readOnly/>
           </Center>
         </Box>
       </Stack>
